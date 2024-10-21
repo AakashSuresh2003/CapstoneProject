e51748db3e9f681efecfbccc4ab60615394473af
Aakash <aakashsuresh455@gmail.com>
2024-10-21 09:58:34 +0530

    update

--- index.js
+++ index.js
@@ -43,6 +43,6 @@ app.use("/api-docs",swaggerUi.serve ,swaggerUi.setup(swaggerDocs,options));
 
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on the http://localhost:${PORT}`);
   console.log(`Swagger UI is running on http://localhost:${PORT}/api-docs`);
 });
